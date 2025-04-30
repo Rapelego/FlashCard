14db7b2c0b7736dc98074200b43ae7e19e525041
LethaboMaditsi <st10496347@rcconnect.edu.za>
2025-04-30 10:00:07 +0200

    Update QuestionScreen

--- app/src/main/java/com/example/flashcard/QuestionScreen.kt
+++ app/src/main/java/com/example/flashcard/QuestionScreen.kt
@@ -1,5 +1,6 @@
 package com.example.flashcard
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -16,9 +17,55 @@ import com.example.flashcard.ui.theme.FlashCardTheme
 class QuestionScreen : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        data class Question(val Question: String, val answer: Boolean)
         setContent {
+//FlashCard Questions and answers
+            val Questions = arrayOf(
+                Question("Nelson Mandela was the first black president in South Africa", true),
+                Question("The Capital of South Africa is Cape Town", false),
+                Question("Jacob Zuma was the president of the Youth League", false),
+                Question("Shaka Zulu was a famous Bapedi leader", false),
+                Question("The apartheid era ended in the 1990's", true)
+            )
+            //Check questions and update score
+            var currentQuestionIndex = 0
+            var score = 0
+
+            fun checkAnswer(answer: Boolean) {
+                if (answer == Questions[currentQuestionIndex].answer) {
+                    score++
+                    val feedbackTextView = null
+                    feedbackTextView.text = "Correct!"
+                } else {
+                    val feedbackTextView = null
+                    feedbackTextView.text = "incorrect!"
+                }
+                currentQuestionIndex++
+                if (currentQuestionIndex < Questions.size) {
+                    displayQuestion()
+                } else {
+                    displayScore()
+                }
+            }
+            fun displayQuestion(){
+                val questionTextView = null
+                questionTextView.text= Questions[currentQuestionIndex].Question
+            }
+            fun displayScore(){
+                val intent= Intent(this, ScoreScreen::class.java)
+                intent.putExtra("score",score)
+                startActivity(intent)
+            }
+
+
+
 
         }
     }
-}
+
+    private fun displayScore() {
+        TODO("Not yet implemented")
+        }
+    }
+
 
