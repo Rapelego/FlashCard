0c9c9276b76840b62bdffe68c23258d73df80897
LethaboMaditsi <st10496347@rcconnect.edu.za>
2025-04-30 09:57:48 +0200

    Welcome Screen

--- app/src/main/java/com/example/flashcard/MainActivity.kt
+++ app/src/main/java/com/example/flashcard/MainActivity.kt
@@ -1,23 +1,46 @@
 package com.example.flashcard
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.flashcard.ui.theme.FlashCardTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            Column {
+                Text(text = "Welcome")
+                Spacer(modifier = Modifier.size(10.dp))
+                Text(text = "This a flash card app to help you study for your History")
+                Divider()
+                Spacer(modifier = Modifier.size(50.dp))
+                Button(onClick = {
+                    //Runs Question Screen When Clicked
+                    val Start = Intent(thiscom.example.flashcardquiz.MainActivity, QuestionScreen::class.java)
+                    startActivity(Start)
+                })
+                {
+                    Text(text = "Start Quiz")
+                }
 
+
+            }
         }
     }
 }
